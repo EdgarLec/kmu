b4c342b78b20ebbc97151cc519010e539d707264
Edgar Lecuyer <edgar.lecuyer@epitech.eu>
2025-05-27 00:29:45 +0900

    Add head metadata and manifest.json for improved PWA support

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
 import Component from "../components/test";
 
 export default function Page() {
-  return <Component />
+  return <>
+  <head>
+    <link rel="manifest" href="/manifest.json"/>
+    <meta name="apple-mobile-web-app-capable" content="yes"/>
+    <meta name="apple-mobile-web-app-status-bar-style" content="black"/>
+    <link rel="apple-touch-icon" href="/icons/icon-192.png"/>
+  </head>
+  <Component />
+  </>
 }
